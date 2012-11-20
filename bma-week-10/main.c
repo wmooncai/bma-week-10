56ddfe791b2f1a58ebbb7fd8688bbc7458f75702
Wallace Mooncai <wmooncai@alumni.rutgers.edu>
2012-11-20 11:47:12 -0500

    Initial Commit

---
+++ bma-week-10/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  bma-week-10
+//
+//  Created by Wally on 11/20/12.
+//  Copyright (c) 2012 W M. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
