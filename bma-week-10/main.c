b43a238c9f8fdccd681e2367c704999ad303dc89
Wallace Mooncai <wmooncai@alumni.rutgers.edu>
2012-11-20 13:55:06 -0500

    Initial Commit - Week 10 Basic HW solution: FutureTime - Chapter 10 (pg 66) Challenge - structs

--- bma-week-10/main.c
+++ bma-week-10/main.c
@@ -7,12 +7,28 @@
 //
 
 #include <stdio.h>
+#include <time.h>
 
 int main(int argc, const char * argv[])
 {
 
-    // insert code here...
-    printf("Hello, World!\n");
+    long currentTime = time(NULL);
+    struct tm cTime;
+    
+    cTime = *localtime(&currentTime);
+    struct tm fTime;
+    
+    printf("                   Current Date: %d-%d-%d\n\n"
+           , (cTime.tm_mon + 1), cTime.tm_mday
+           , (cTime.tm_year + 1900));
+    
+    currentTime += 4000000;
+    fTime = *localtime(&currentTime);
+    
+    printf("Future Date (4,000,000 seconds): %d-%d-%d\n"
+           , (fTime.tm_mon + 1), fTime.tm_mday
+           , (fTime.tm_year + 1900));
+    
     return 0;
 }
 
