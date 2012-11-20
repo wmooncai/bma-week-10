d1df8af429e417d23bc3266f14278af3cb5f01a8
Wallace Mooncai <wmooncai@alumni.rutgers.edu>
2012-11-20 18:58:26 -0500

    Functionized the code.  Refactored some of the variable names.  Added capability to output date and / or time.

---
+++ bma-week-10/bma-week-10.h
@@ -0,0 +1,22 @@
+//
+//  bma-week-10.h
+//  bma-week-10
+//
+//  Created by Wally on 11/20/12.
+//  Copyright (c) 2012 W M. All rights reserved.
+//
+
+#ifndef bma_week_10_bma_week_10_h
+#define bma_week_10_bma_week_10_h
+
+char* printTime(char *, long , BOOL , BOOL);
+
+#define SUCCESS 0
+#define FAIL -1
+
+#define DATE_FMT "%d-%d-%d"
+#define TIME_FMT "%d:%d:%d %s"
+#define DATE_TIME_FMT "%d-%d-%d %d:%d:%d %s"
+
+
+#endif
--- bma-week-10/main.c
+++ bma-week-10/main.c
@@ -2,33 +2,87 @@
 //  main.c
 //  bma-week-10
 //
-//  Created by Wally on 11/20/12.
+//  Created by W. Mooncai on 11/20/12.
 //  Copyright (c) 2012 W M. All rights reserved.
 //
 
 #include <stdio.h>
+#include <string.h>
 #include <time.h>
+#include <objc/objc.h>
+
+#include "bma-week-10.h"
+
+// *****************************************************************************
+
+int printDateTime(char *message, long timeValue, BOOL date, BOOL time)
+{
+    struct tm compositeTime = *localtime(&timeValue);
+    
+    char printFormat[strlen(message) + sizeof(DATE_TIME_FMT)];
+    strlcpy(printFormat, message, sizeof(printFormat));
+    
+    if (date && !time) {
+        printf(strncat(printFormat, DATE_FMT
+                       , ( sizeof(printFormat) - strlen(DATE_FMT) - 1 ))
+               , (compositeTime.tm_mon + 1)
+               , compositeTime.tm_mday
+               , (compositeTime.tm_year + 1900));
+        
+    } else if (!date && time) {
+        printf(strncat(printFormat, TIME_FMT
+                       , ( sizeof(printFormat) - strlen(TIME_FMT) - 1 ))
+               , compositeTime.tm_hour
+               , compositeTime.tm_min
+               , compositeTime.tm_sec);
+        
+    } else if (date && time) {
+        printf(strncat(printFormat, DATE_TIME_FMT
+                       , ( sizeof(printFormat) - strlen(DATE_TIME_FMT) - 1 ))
+               
+               , (compositeTime.tm_mon + 1)
+               , compositeTime.tm_mday
+               , (compositeTime.tm_year + 1900)
+               , ( (compositeTime.tm_hour > 12)
+                  ? (compositeTime.tm_hour - 12) : compositeTime.tm_hour )
+               , compositeTime.tm_min
+               , compositeTime.tm_sec
+               , ( (compositeTime.tm_hour > 12) ? "PM" : "AM") );
+        
+    } else return FAIL;
+    
+    printf("\n");
+    
+    return SUCCESS;
+}
+
+// *****************************************************************************
 
 int main(int argc, const char * argv[])
 {
-
-    long currentTime = time(NULL);
-    struct tm cTime;
     
-    cTime = *localtime(&currentTime);
-    struct tm fTime;
+    long timeValue = time(NULL);
     
-    printf("                   Current Date: %d-%d-%d\n\n"
-           , (cTime.tm_mon + 1), cTime.tm_mday
-           , (cTime.tm_year + 1900));
+    printDateTime("                    Current Date: "
+                  , timeValue, YES, NO);
     
-    currentTime += 4000000;
-    fTime = *localtime(&currentTime);
+    printDateTime("Future Date (+4,000,000 seconds): "
+                  , (timeValue + 4000000), YES, NO);
     
-    printf("Future Date (4,000,000 seconds): %d-%d-%d\n"
-           , (fTime.tm_mon + 1), fTime.tm_mday
-           , (fTime.tm_year + 1900));
+    printf("\n------------------\n\n");
     
-    return 0;
+    printDateTime("                    Current Date/Time: "
+                  , timeValue, YES, YES);
+    
+    printDateTime("Future Date/Time (+4,000,000 seconds): "
+                  , (timeValue + 4000000), YES, YES);
+    
+    printf("\n------------------\n\n");
+    
+    if (printDateTime("No Date: "
+                      , timeValue, NO, NO) == FAIL) {
+        printf("ERROR: You must output date and/or time, but not neither.");
+    };
+    
+    return SUCCESS;
 }
-
